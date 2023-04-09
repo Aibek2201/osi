87e46432f6e14f201e098947cb1050fa522642f3
Madi <abaymadi1@gmail.com>
2023-04-09 16:36:28 +0600

    users app добавлен

--- src/settings.py
+++ src/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     #local
     'houses',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
